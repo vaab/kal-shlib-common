cc6c7b731106270c8d04c51973411420d0e91e48
Valentin Lab <valentin.lab@kalysto.org>
2015-11-06 11:17:50 +0800

    new: [parse] added ``is_{float,num,uint,bash_int,int}`` string numeric representation checks.

--- src/lib/libparse.sh
+++ src/lib/libparse.sh
@@ -32,4 +32,11 @@ function remove() {
 
 }
 
+function is_uint () {  [[ "$1" =~ ^[0-9]+$ ]] ; }
+function is_int () {  [[ "$1" =~ ^-?[0-9]+$ ]] ; }
+function is_bash_int () {  [ "$1" -eq "$1" ] 2>/dev/null ; }
+function is_float () {  [[ "$1" =~ ^-?[0-9]*(.[0-9]*)?$ ]] ; }
+function is_num () { is_float "$1" ; }
+
+
 ## End libparse.sh
