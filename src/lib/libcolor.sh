3067eaba07511d2f8de564baf14050e268076e89
Valentin Lab <valentin.lab@kalysto.org>
2016-05-24 11:14:21 +0200

    chg: [color] prevents loads of unnecessary sub-shells on loading.

--- src/lib/libcolor.sh
+++ src/lib/libcolor.sh
@@ -15,9 +15,8 @@ if [ -z "$COLUMNS" ]; then
     ## Get the console size (rows columns)
 
     stty size > /dev/null 2>&1
-    if [ "$?" == 0 ]
-    then
-        [ "$CONSOLE" == "/dev/console" ] && SIZE=$(stty size < $CONSOLE) \
+    if [ "$?" = 0 ]; then
+        [ "$CONSOLE" = "/dev/console" ] && SIZE=$(stty size < $CONSOLE) \
             || SIZE=$(stty size)
 
         ## Strip off the rows leaving the columns
@@ -37,10 +36,10 @@ SEP_ELT_INFO=" "
 SEP_INFO_STATUS=" "
 SEP_STATUS_CHAR=" "
 
-SEP_LIST_ELT_SIZE=$(echo -n "$SEP_LIST_ELT" | wc -c)
-SEP_ELT_INFO_SIZE=$(echo -n "$SEP_ELT_INFO" | wc -c)
-SEP_INFO_STATUS_SIZE=$(echo -n "$SEP_INFO_STATUS" | wc -c)
-SEP_STATUS_CHAR_SIZE=$(echo -n "$SEP_STATUS_CHAR" | wc -c)
+SEP_LIST_ELT_SIZE=${#SEP_LIST_ELT}
+SEP_ELT_INFO_SIZE=${#SEP_ELT_INFO}
+SEP_INFO_STATUS_SIZE=${#SEP_INFO_STATUS}
+SEP_STATUS_CHAR_SIZE=${#SEP_STATUS_CHAR}
 
 
 SIZE_LINE=$COLUMNS                            ## full line size
@@ -78,39 +77,39 @@ ansi_color() {
 
     if [ "$choice" != "no" ]; then
 
-        SET_COL_CHAR=$(echo -en "\e[${COL_CHAR}G")
-        SET_COL_STATUS=$(echo -en "\e[${COL_STATUS}G")
-        SET_COL_INFO=$(echo -en "\e[${COL_INFO}G")
-        SET_COL_ELT=$(echo -en "\e[${COL_ELT}G")
+        SET_COL_CHAR="${ANSI_ESC}${COL_CHAR}G"
+        SET_COL_STATUS="${ANSI_ESC}${COL_STATUS}G"
+        SET_COL_INFO="${ANSI_ESC}${COL_INFO}G"
+        SET_COL_ELT="${ANSI_ESC}${COL_ELT}G"
 
-        SET_BEGINCOL=$(echo -en "\e[0G")
+        SET_BEGINCOL="${ANSI_ESC}0G"
 
-        UP=$(echo -en "\e[1A")
-        DOWN=$(echo -en "\e[1B")
-        LEFT=$(echo -en "\e[1D")
-        RIGHT=$(echo -en "\e[1C")
+        UP="${ANSI_ESC}1A"
+        DOWN="${ANSI_ESC}1B"
+        LEFT="${ANSI_ESC}1D"
+        RIGHT="${ANSI_ESC}1C"
 
-        SAVE=$(echo -en "\e7")
-        RESTORE=$(echo -en "\e8")
+        SAVE="${ANSI_ESC}7"
+        RESTORE="${ANSI_ESC}8"
 
-        NORMAL=$(echo -en "\e[0m")
+        NORMAL="${ANSI_ESC}0m"
 
-        GRAY=$(echo -en "\e[1;30m")
-        RED=$(echo -en "\e[1;31m")
-        GREEN=$(echo -en "\e[1;32m")
-        YELLOW=$(echo -en "\e[1;33m")
-        BLUE=$(echo -en "\e[1;34m")
-        PINK=$(echo -en "\e[1;35m")
-        CYAN=$(echo -en "\e[1;36m")
-        WHITE=$(echo -en "\e[1;37m")
+        GRAY="${ANSI_ESC}1;30m"
+        RED="${ANSI_ESC}1;31m"
+        GREEN="${ANSI_ESC}1;32m"
+        YELLOW="${ANSI_ESC}1;33m"
+        BLUE="${ANSI_ESC}1;34m"
+        PINK="${ANSI_ESC}1;35m"
+        CYAN="${ANSI_ESC}1;36m"
+        WHITE="${ANSI_ESC}1;37m"
 
-        DARKGRAY=$(echo -en "\e[0;30m")
-        DARKRED=$(echo -en "\e[0;31m")
-        DARKGREEN=$(echo -en "\e[0;32m")
-        DARKYELLOW=$(echo -en "\e[0;33m")
-        DARKBLUE=$(echo -en "\e[0;34m")
-        DARKPINK=$(echo -en "\e[0;35m")
-        DARKCYAN=$(echo -en "\e[0;36m")
+        DARKGRAY="${ANSI_ESC}0;30m"
+        DARKRED="${ANSI_ESC}0;31m"
+        DARKGREEN="${ANSI_ESC}0;32m"
+        DARKYELLOW="${ANSI_ESC}0;33m"
+        DARKBLUE="${ANSI_ESC}0;34m"
+        DARKPINK="${ANSI_ESC}0;35m"
+        DARKCYAN="${ANSI_ESC}0;36m"
 
         SUCCESS=$GREEN
         WARNING=$YELLOW
