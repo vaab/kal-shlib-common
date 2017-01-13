5579e85997938a7f8b1d311b24ea4d1b630ffcde
Valentin Lab <valentin.lab@kalysto.org>
2017-01-13 10:11:04 +0800

    chg: dev: removed ``function`` incompatible keyword. !minor

--- src/lib/libparse.sh
+++ src/lib/libparse.sh
@@ -42,11 +42,11 @@ read-0() {
     test "$eof" != true
 }
 
-function is_uint () {  [[ "$1" =~ ^[0-9]+$ ]] ; }
-function is_int () {  [[ "$1" =~ ^-?[0-9]+$ ]] ; }
-function is_bash_int () {  [ "$1" -eq "$1" ] 2>/dev/null ; }
-function is_float () {  [[ "$1" =~ ^-?[0-9]*(.[0-9]*)?$ ]] ; }
-function is_num () { is_float "$1" ; }
 
+is_uint () {  [[ "$1" =~ ^[0-9]+$ ]] ; }
+is_int () {  [[ "$1" =~ ^-?[0-9]+$ ]] ; }
+is_bash_int () {  [ "$1" -eq "$1" ] 2>/dev/null ; }
+is_float () {  [[ "$1" =~ ^-?[0-9]*(.[0-9]*)?$ ]] ; }
+is_num () { is_float "$1" ; }
 
 ## End libparse.sh
